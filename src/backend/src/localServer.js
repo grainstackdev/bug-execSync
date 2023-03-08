7e36eccde2e26f3f1d27a934697d709723500593
grainstackdev <99065516+grainstackdev@users.noreply.github.com>
2023-03-08 04:15:38 -0800

    simplified

--- src/backend/src/localServer.js
+++ src/backend/src/localServer.js
@@ -1,38 +1,27 @@
-import express from "express"
 import {execSync} from "child_process";
 
-const app = express()
-
-app.use(express.json())
-
-async function start() {
-  const command = `
-    rm -rf /tmp/postgres-local
-    mkdir -p /tmp/postgres-local/data
-    initdb -D /tmp/postgres-local/data
-    pg_ctl -D /tmp/postgres-local/data -o "-F -p 5555" -l /tmp/postgres-local/logfile start
-  `
-  execSync(command)
+function start() {
+  setTimeout(() => {
+    start()
+  }, 1000)
 }
 
 function stopFails() {
-  const command = `pg_ctl stop -D /tmp/postgres-local/data`
+  const command = `npm -v`
   execSync(command)
   console.log('postgres server stopped')
 }
 
 function stopWorks() {
   const command = `
-  pg_ctl stop -D /tmp/postgres-local/data
+  npm -v
   `
   execSync(command)
   console.log('postgres server stopped')
 }
 
-
-
-await start()
-app.listen(4000)
+start()
+// app.listen(4000)
 console.log('server started on 4000')
 process.on('SIGINT', () => {
   console.log("stopping");
